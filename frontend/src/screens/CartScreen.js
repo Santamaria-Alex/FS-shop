b65eb39b62eb92eac1e4c8a6bbd784eb2db43d34
Alex Santamaria <santamariaalex146@gmail.com>
2022-04-08 17:34:40 -0500

    broke the app

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -15,22 +15,23 @@ import { addToCart } from "../actions/cartActions";
 import { useLocation, useParams } from "react-router";
 
 const CartScreen = () => {
-  //   const match = useParams();
-  //   const productID = match.id;
-  //   console.log(productID);
-  //   //   console.log(id);
-
-  //   const { search } = useLocation().search;
-  //   const name = new URLSearchParams(search).get("name");
-  //   //   console.log(name);
-  //   console.log(search);
-
+  //get item id
   const { id } = useParams();
   console.log(id);
 
+  //get qty inputed from user
   const [searchParams] = useSearchParams();
   const qty = Number(searchParams.get("qty"));
   console.log(qty);
+
+  //useDispatch is used to dispatch an action, while useSelector is used to get the state from the redux store
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (id) {
+      dispatch(addToCart(id, qty));
+    }
+  }, [dispatch, id, qty]); //dependencies: if either change, useEffect fires off
 
   return <div>CartScreen</div>;
 };
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -16,9 +16,9 @@ const reducer = combineReducers({
 
 //if theres a 'cartItems' in localStorage then we get the data and parse it bc its a string
 //if there isnt then set an empty array
-const cartItemsFromStorage = localStorage.getItem("cartItems")
-  ? JSON.parse(localStorage.getItem("cartItems"))
-  : [];
+const cartItemsFromStorage = localStorage.getItem("cartItems") && if (localStorage.getItem("cartItems") !== 'undefined') {
+  JSON.parse(localStorage.getItem("cartItems"))
+} else [];
 
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
