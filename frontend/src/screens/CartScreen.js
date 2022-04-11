76a07e9cfd69c1aeb0cb4f056895184ed5492820
Alex Santamaria <santamariaalex146@gmail.com>
2022-04-11 17:31:14 -0500

    able to display items added to cart in cartScreen from state, working on delete btn and subtotal items

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -2,12 +2,15 @@ import React, { useEffect } from "react";
 import { Link, useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
+  Button,
   ButtonGroup,
   Card,
   Col,
   Form,
+  FormControl,
   Image,
   ListGroup,
+  ListGroupItem,
   Row,
 } from "react-bootstrap";
 import Message from "../components/Message";
@@ -38,7 +41,78 @@ const CartScreen = () => {
     }
   }, [dispatch, id, qty]); //dependencies: if either change, useEffect fires off
 
-  return <div>CartScreen</div>;
+  const removeFromCartHandler = (id) => {
+    console.log(id);
+  };
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to="/">Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroupItem key={item.product}>
+                <Row>
+                  <Col md={2} sm={2} xs={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3} sm={3} xs={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2} sm={2} xs={2}>
+                    ${item.price}
+                  </Col>
+                  <Col md={2} sm={2} xs={2}>
+                    <FormControl
+                      as="select"
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(
+                          addToCart(item.product, Number(e.target.value))
+                        )
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </FormControl>
+                  </Col>
+                  <Col md={2} sm={2} xs={2}>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className="fas fa-trash"></i>
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroupItem>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2>
+            </ListGroupItem>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default CartScreen;
