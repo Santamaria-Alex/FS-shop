b579dcb50fa19a5bd1538e1ff567f579daad3e7a
Alex Santamaria <santamariaalex146@gmail.com>
2022-04-11 15:41:46 -0500

    fixed the bug

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -16,9 +16,16 @@ const reducer = combineReducers({
 
 //if theres a 'cartItems' in localStorage then we get the data and parse it bc its a string
 //if there isnt then set an empty array
-const cartItemsFromStorage = localStorage.getItem("cartItems") && if (localStorage.getItem("cartItems") !== 'undefined') {
-  JSON.parse(localStorage.getItem("cartItems"))
-} else [];
+// const cartItemsFromStorage = localStorage.getItem("cartItems")
+//   ? JSON.parse(localStorage.getItem("cartItems"))
+//   : [];
+
+//above equation didn't work for undefined local storage bc u can't parse undefined
+const lsTrue = localStorage.getItem("cartItems");
+const lsUndefinded = localStorage.getItem("cartItems") !== "undefined";
+
+const cartItemsFromStorage =
+  lsTrue && lsUndefinded ? JSON.parse(localStorage.getItem("cartItems")) : [];
 
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
