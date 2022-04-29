798c5abf92d83df94416936b50942aa99f828ae6
Alex Santamaria <santamariaalex146@gmail.com>
2022-04-29 16:11:39 -0500

    created order controller to save order to database

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,44 @@
+const asyncHandler = require("express-async-handler");
+const Order = require("../models/orderModel");
+
+// @desc    Create new order
+// @route   POST /api/orders
+// @access  Private
+//get all products from products.js
+//async handler is middleware for handling exceptions
+const addOrderItems = asyncHandler(async (req, res) => {
+  //get from order model/schema
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  //check that orderItems isn't empty
+  if (orderItems && orderItems.length === 0) {
+    res.status(400); //bad request
+    throw new Error("No order items.");
+  } else {
+    //instantiate new order
+    const order = new Order({
+      orderItems,
+      user: req.user._id, //attach logged in user
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    //save to database
+    const createdOrder = await order.save();
+
+    //201: something was created
+    res.status(201).json(createdOrder);
+  }
+});
