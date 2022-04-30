94470a8a43598788e51b459a9025a394533799c6
Alex Santamaria <santamariaalex146@gmail.com>
2022-04-30 16:25:14 -0500

    created action to get data from backend

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -3,6 +3,9 @@ import {
   ORDER_CREATE_REQUEST,
   ORDER_CREATE_FAIL,
   ORDER_CREATE_SUCCESS,
+  ORDER_DETAILS_REQUEST,
+  ORDER_DETAILS_SUCCESS,
+  ORDER_DETAILS_FAIL,
 } from "../constants/orderConstants";
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -35,6 +38,46 @@ export const createOrder = (order) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: ORDER_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+/////////////////////////////////////////////////////////////////
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try {
+    //send request
+    dispatch({
+      type: ORDER_DETAILS_REQUEST,
+    });
+
+    //destructure userInfo from userLogin, which is destructured from getState function
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    //pass in token into headers
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    //get data from backend that matches id passed in as parameter
+    const { data } = await axios.get(`/api/orders${id}`, config);
+
+    //send data
+    dispatch({
+      type: ORDER_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
