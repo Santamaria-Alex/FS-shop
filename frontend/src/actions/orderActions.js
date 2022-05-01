2b229547a34da2e90bb55b62c5adef422ea22448
Alex Santamaria <santamariaalex146@gmail.com>
2022-04-30 20:18:57 -0500

    created payment action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -6,6 +6,9 @@ import {
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
   ORDER_DETAILS_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
+  ORDER_PAY_FAIL,
 } from "../constants/orderConstants";
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -85,3 +88,49 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
     });
   }
 };
+
+/////////////////////////////////////////////////////////////////
+
+export const payOrder =
+  (orderId, paymentResult) => async (dispatch, getState) => {
+    try {
+      //send request
+      dispatch({
+        type: ORDER_PAY_REQUEST,
+      });
+
+      //destructure userInfo from userLogin, which is destructured from getState function
+      const {
+        userLogin: { userInfo },
+      } = getState();
+
+      //pass in token into headers
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+
+      //update payment data from backend that matches id
+      const { data } = await axios.put(
+        `/api/orders/${orderId}/pay`,
+        paymentResult,
+        config
+      );
+
+      //send data
+      dispatch({
+        type: ORDER_PAY_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: ORDER_PAY_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+    }
+  };
