54a50dbfb6cb1ad7c071f3e8a77d2885ac92b39e
Alex Santamaria <santamariaalex146@gmail.com>
2022-04-30 20:08:35 -0500

    created route to update whether order has been paid

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -70,4 +70,35 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { addOrderItems, getOrderById };
+/////////////////////////////////////////////////////////////////
+
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+//async handler is middleware for handling exceptions
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  //fetch order
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    //this comes from paypal
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    //save the order to the database
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found!");
+  }
+});
+
+module.exports = { addOrderItems, getOrderById, updateOrderToPaid };
