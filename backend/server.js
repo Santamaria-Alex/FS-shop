56a8a58ad85288957150397a30c6140b8ba78613
Alex Santamaria <santamariaalex146@gmail.com>
2022-05-02 14:12:35 -0500

    created paypal sandbox acct and applied route to backend

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,11 @@ app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 
+//connect to paypal
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
 //error handler for "not found" or 404,
 app.use(notFound);
 
