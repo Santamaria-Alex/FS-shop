f1b44b8584da0c56292cc6a930b0252e8868bd53
Alex Santamaria <santamariaalex146@gmail.com>
2022-05-02 18:45:45 -0500

    created action to get data from backend to frontend

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -9,6 +9,9 @@ import {
   ORDER_PAY_REQUEST,
   ORDER_PAY_SUCCESS,
   ORDER_PAY_FAIL,
+  ORDER_LIST_MY_REQUEST,
+  ORDER_LIST_MY_SUCCESS,
+  ORDER_LIST_MY_FAIL,
 } from "../constants/orderConstants";
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -134,3 +137,43 @@ export const payOrder =
       });
     }
   };
+
+/////////////////////////////////////////////////////////////////
+
+export const listMyOrders = () => async (dispatch, getState) => {
+  try {
+    //send request
+    dispatch({
+      type: ORDER_LIST_MY_REQUEST,
+    });
+
+    //destructure userInfo from userLogin, which is destructured from getState function
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    //pass in token into headers
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    //get data from backend
+    const { data } = await axios.get(`/api/orders/myorders`, config);
+
+    //send data
+    dispatch({
+      type: ORDER_LIST_MY_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_LIST_MY_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
