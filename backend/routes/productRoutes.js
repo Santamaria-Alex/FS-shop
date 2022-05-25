b652cf781b23ec5f9384db20c7c93278ad35453a
Alex Santamaria <santamariaalex146@gmail.com>
2022-05-25 15:48:27 -0500

    able to delete products from backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const {
   getProductById,
   getProducts,
+  deleteProduct,
 } = require("../controllers/productController");
 const router = express.Router();
+const { protect, admin } = require("../middleware/authMiddleware");
 
 router.route("/").get(getProducts);
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct);
 
 module.exports = router;
