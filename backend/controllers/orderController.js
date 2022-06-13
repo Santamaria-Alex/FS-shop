8f1f7446b6a2676ad10c243522ef3b8558696e86
Alex Santamaria <santamariaalex146@gmail.com>
2022-06-13 15:54:48 -0500

    created route in controller to update delivery status

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -127,10 +127,35 @@ const getOrders = asyncHandler(async (req, res) => {
   res.json(orders);
 });
 
+/////////////////////////////////////////////////////////////////
+
+// @desc    Update order to delivered
+// @route   GET /api/orders/:id/deliver
+// @access  Private/Admin
+//async handler is middleware for handling exceptions
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  //fetch order
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+
+    //save the order to the database
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found!");
+  }
+});
+
 module.exports = {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
   getMyOrders,
   getOrders,
+  updateOrderToDelivered,
 };
