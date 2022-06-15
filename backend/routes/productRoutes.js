0c9abc1e9a28e9cd2c166a4044f721a45408ad48
Alex Santamaria <santamariaalex146@gmail.com>
2022-06-15 15:00:51 -0500

    backend func that allows users to review products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,11 +5,13 @@ const {
   deleteProduct,
   updateProduct,
   createProduct,
+  createProductReview,
 } = require("../controllers/productController");
 const router = express.Router();
 const { protect, admin } = require("../middleware/authMiddleware");
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 router
   .route("/:id")
   .get(getProductById)
