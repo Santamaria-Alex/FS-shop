085d3d8099e263f0ce7f91c1a5813945dc7d77e1
Alex Santamaria <santamariaalex146@gmail.com>
2022-07-01 17:59:51 -0500

    working on creating a caarousel w top 3 rated products, backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -166,6 +166,17 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 });
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// @desc    Get top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+
+  res.json(products);
+});
+
 // export { getProducts, getProductById };
 module.exports = {
   getProducts,
@@ -174,4 +185,5 @@ module.exports = {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,12 +6,14 @@ const {
   updateProduct,
   createProduct,
   createProductReview,
+  getTopProducts,
 } = require("../controllers/productController");
 const router = express.Router();
 const { protect, admin } = require("../middleware/authMiddleware");
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.route("/:id/reviews").post(protect, createProductReview);
+router.get("/top", getTopProducts);
 router
   .route("/:id")
   .get(getProductById)
